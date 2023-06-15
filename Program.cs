2d949f771ad5d2c3012cd916ce7d6eaf7d87a610
sanjana8100 <sanjanams812@gmail.com>
2023-06-15 18:53:52 +0530

    added: Review 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DND_Review1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 8, 5, 1, 7, 6, 4, 3 };
+            for (int index = array.Length-1 ; index >= 0; index--)
+            {
+                if (array[index]%2 != 0)
+                {
+                    Console.WriteLine(array[index]);
+                    break;
+                }
+            }
+        }
+    }
+}
